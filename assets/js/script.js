358d8c0b074463be23823ae63a584fa1866aa72d
Nate Pfau <natepfau@yahoo.com>
2021-06-06 18:52:07 -0500

    Save prevous searches and have them search when clicking their buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var iconUrl = "http://openweathermap.org/img/wn/{var}@2x.png"
 var city = "";
 var currentWeatherDay = null;
 var futureWeatherDays = [];
+var previousSearches = [];
 
 function WeatherDay(temp, windSpeed, humidity, uvIndex, icon, time)
 {
@@ -16,7 +17,6 @@ function WeatherDay(temp, windSpeed, humidity, uvIndex, icon, time)
 
 function getCurrentWeatherData(searchTerm)
 {
-    city = searchTerm;
     var apiString = "https://api.openweathermap.org/data/2.5/weather?appid=" + apiKey + "&q=" + searchTerm.trim();
 
     fetch(apiString)
@@ -34,6 +34,8 @@ function getCurrentWeatherData(searchTerm)
         .then(function(data)
         {
             console.log(data);
+            city = data.name;
+
             var lat = data.coord.lat;
             var lon = data.coord.lon;
 
@@ -64,10 +66,10 @@ function getOneCallWeatherData(lat, lon)
         .then(function(data)
         {
             console.log(data);
-
             currentWeatherDay = new WeatherDay(data.current.temp, data.current.wind_speed, data.current.humidity, data.current.uvi, data.current.weather[0].icon, data.current.dt);
             var currentTime = moment(currentWeatherDay.time, "X");
             console.log(currentTime.format("dddd MM/DD/YYYY"));
+            futureWeatherDays = [];
             for(var i = 1; i < data.daily.length; i++)
             {
                 var weatherDay = new WeatherDay(data.daily[i].temp.max, data.daily[i].wind_speed, data.daily[i].humidity, data.daily[i].uvi, data.daily[i].weather[0].icon, data.daily[i].dt);
@@ -86,12 +88,11 @@ function getOneCallWeatherData(lat, lon)
 
 function displayData()
 {
-    var imgSrc = "http://openweathermap.org/img/wn/{0}@2x.png";
     //Create Current Weather Card
     var cardBody = $("<div>").addClass("card-body");
     var currentTime = moment(currentWeatherDay.time, "X");
     var cardHeader = $("<h3>").addClass("card-title").text(city + currentTime.format(" (dddd MM/DD/YYYY)"));
-    var cardImg = $("<img>").attr("src", imgSrc.replace("{0}", currentWeatherDay.icon));
+    var cardImg = $("<img>").attr("src", iconUrl.replace("{var}", currentWeatherDay.icon));
     var cardTemp = $("<p>").addClass("card-text").text("Temp: " + currentWeatherDay.temp);
     var cardWindSpeed = $("<p>").addClass("card-text").text("Wind Speed: " + currentWeatherDay.windSpeed);
     var cardHumidity = $("<p>").addClass("card-text").text("Humidity: " + currentWeatherDay.humidity);
@@ -110,7 +111,7 @@ function displayData()
         var currentTime = moment(futureWeatherDays[i].time, "X");
         var cardHeader = $("<h4>").addClass("card-title").text(currentTime.format("dddd"));
         var cardDate = $("<h5>").addClass("card-text").text(currentTime.format("(MM/DD/YYYY)"));
-        var cardImg = $("<img>").attr("src", imgSrc.replace("{0}", futureWeatherDays[i].icon));
+        var cardImg = $("<img>").attr("src", iconUrl.replace("{var}", futureWeatherDays[i].icon));
         var cardTemp = $("<p>").addClass("card-text").text("Temp: " + futureWeatherDays[i].temp);
         var cardWindSpeed = $("<p>").addClass("card-text").text("Wind Speed: " + futureWeatherDays[i].windSpeed);
         var cardHumidity = $("<p>").addClass("card-text").text("Humidity: " + futureWeatherDays[i].humidity);
@@ -120,6 +121,61 @@ function displayData()
         cardColumn.append(cardBody);
         $(".future-weather").append(cardColumn);
     }
+
+    //Add search to previous search list
+    for(var i = 0; i < previousSearches.length; i++)
+    {
+        if(previousSearches[i] == city)
+        {
+            //Already in previous searches
+            return;
+        }
+    }
+    if(previousSearches.length >= 10)
+        previousSearches.shift();
+    previousSearches.push(city);
+
+    DisplaySearchList();
 }
 
-getCurrentWeatherData("Duluth");
+function DisplaySearchList()
+{
+    $(".prevous-list").empty();
+    for(var i = 0; i < previousSearches.length; i++)
+    {
+        var listItem = $("<button>").addClass("list-group-item list-group-item-secondary text-center mt-1").text(previousSearches[i]);
+        $(".prevous-list").append(listItem);
+    }
+
+    localStorage.setItem("previousSearches", JSON.stringify(previousSearches));
+}
+
+function OnLoad()
+{
+    previousSearches = JSON.parse(localStorage.getItem("previousSearches"));
+
+    if(!previousSearches)
+    {
+        previousSearches = [];
+        getCurrentWeatherData("Duluth");
+    }
+    else
+    {
+        getCurrentWeatherData(previousSearches[0]);
+        DisplaySearchList();
+    }
+}
+$(".form-input").on("submit", function (event) 
+{
+    event.preventDefault();
+
+    getCurrentWeatherData($(".search-input").val());
+    $(".search-input").val("");
+});
+
+$(".prevous-list").on("click", "button", function () 
+{
+    getCurrentWeatherData($(this).text());
+});
+
+OnLoad();
