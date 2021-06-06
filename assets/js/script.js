ed27624f9141b87144367fef2b548b04235e91a4
Nate Pfau <natepfau@yahoo.com>
2021-06-06 17:40:47 -0500

    Set data from fetch call to html components

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var apiKey = "d248e86769ff3a40ac703128046316a7";
 var iconUrl = "http://openweathermap.org/img/wn/{var}@2x.png"
+var city = "";
 var currentWeatherDay = null;
 var futureWeatherDays = [];
 
@@ -15,6 +16,7 @@ function WeatherDay(temp, windSpeed, humidity, uvIndex, icon, time)
 
 function getCurrentWeatherData(searchTerm)
 {
+    city = searchTerm;
     var apiString = "https://api.openweathermap.org/data/2.5/weather?appid=" + apiKey + "&q=" + searchTerm.trim();
 
     fetch(apiString)
@@ -84,7 +86,40 @@ function getOneCallWeatherData(lat, lon)
 
 function displayData()
 {
+    var imgSrc = "http://openweathermap.org/img/wn/{0}@2x.png";
+    //Create Current Weather Card
+    var cardBody = $("<div>").addClass("card-body");
+    var currentTime = moment(currentWeatherDay.time, "X");
+    var cardHeader = $("<h3>").addClass("card-title").text(city + currentTime.format(" (dddd MM/DD/YYYY)"));
+    var cardImg = $("<img>").attr("src", imgSrc.replace("{0}", currentWeatherDay.icon));
+    var cardTemp = $("<p>").addClass("card-text").text("Temp: " + currentWeatherDay.temp);
+    var cardWindSpeed = $("<p>").addClass("card-text").text("Wind Speed: " + currentWeatherDay.windSpeed);
+    var cardHumidity = $("<p>").addClass("card-text").text("Humidity: " + currentWeatherDay.humidity);
+    var cardUVIndex = $("<p>").addClass("card-text").text("UV Index: " + currentWeatherDay.uvIndex);
 
+    cardBody.append(cardHeader, cardImg, cardTemp, cardWindSpeed, cardHumidity, cardUVIndex);
+    $(".current-weather").empty();
+    $(".current-weather").append(cardBody);
+
+    $(".future-weather").empty();
+    for(var i = 0; i < 5; i++)
+    {
+        //Create Future Weather Card
+        var cardColumn = $("<div>").addClass("card col-2 m-2 bg-info");
+        var cardBody = $("<div>").addClass("card-body");
+        var currentTime = moment(futureWeatherDays[i].time, "X");
+        var cardHeader = $("<h4>").addClass("card-title").text(currentTime.format("dddd"));
+        var cardDate = $("<h5>").addClass("card-text").text(currentTime.format("(MM/DD/YYYY)"));
+        var cardImg = $("<img>").attr("src", imgSrc.replace("{0}", futureWeatherDays[i].icon));
+        var cardTemp = $("<p>").addClass("card-text").text("Temp: " + futureWeatherDays[i].temp);
+        var cardWindSpeed = $("<p>").addClass("card-text").text("Wind Speed: " + futureWeatherDays[i].windSpeed);
+        var cardHumidity = $("<p>").addClass("card-text").text("Humidity: " + futureWeatherDays[i].humidity);
+        var cardUVIndex = $("<p>").addClass("card-text").text("UV Index: " + futureWeatherDays[i].uvIndex);
+
+        cardBody.append(cardHeader, cardDate, cardImg, cardTemp, cardWindSpeed, cardHumidity, cardUVIndex);
+        cardColumn.append(cardBody);
+        $(".future-weather").append(cardColumn);
+    }
 }
 
 getCurrentWeatherData("Duluth");
